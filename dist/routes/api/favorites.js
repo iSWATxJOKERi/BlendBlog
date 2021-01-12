a73af6c02adfefe680cb1493a63e60e818019c48
swatjoker <iSWATxJOKERi>
2021-01-12 12:19:52 -0500

    Added dist folder to gitignore

--- dist/routes/api/favorites.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const favorites_controller_1 = require("../../controllers/favorites.controller");
-const router = express_1.Router();
-router.post("/favorites/create", favorites_controller_1.createFavorite);
-router.delete("/favorites/delete", favorites_controller_1.deleteFavorite);
-exports.default = router;
--- dist/validation/validText.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const validText = (str) => {
-    return typeof str === 'string' && str.trim().length > 0;
-};
-exports.default = validText;
